2f67cd5b71f31ec1c02fdbe755a2ebaf25bb0429
holkarajitgyandev <139675885+holkarajitgyandev@users.noreply.github.com>
2024-08-04 12:42:14 +0530

    Updated Jobs.jsx

--- glassdoor/src/Pages/Jobs.jsx
+++ glassdoor/src/Pages/Jobs.jsx
@@ -3,85 +3,83 @@ import AuthNavbar from '../Component/AuthNavbar'
 import { baseapi } from '../api/axiosfile'
 import axios from 'axios'
 import Jobcard from '../Component/Jobcard'
+import Jobdetails from '../Component/Jobdetails'
+import { useDispatch } from 'react-redux'
+import { selectjob } from '../store/jobslice/Jobslice'
+
 
 
 const Jobs = () => {
   const [jobs, setjobs] =useState([]);
-  
+  const [selectedJob, setSelectedJob] = useState(null);
+  const dispatch=useDispatch();
 
-  //  const allJobs=async()=>{
-  //   try {
-  //     const res=await baseApi.get("/search",{
-  //       params:{
-  //         query:'Node.js developer in New-York,USA',
-      
-  //       }
-  //     })
-  //     setjobs(res.data)
-      
-  //   } catch (error) {
-  //     console.error('error in fetcing data',error)
-  //   }
-  // }
-  // useEffect(()=>{allJobs()},[]);
-
-  // console.log('MYJOBS',jobs)
-  // const options = {
-  //   method: 'GET',
-  //   url: 'https://jsearch.p.rapidapi.com/search',
-  //   params: {
-  //     query: 'Node.js developer in New-York,USA',
-  //     page: '1',
-  //     num_pages: '1',
-  //     date_posted: 'all'
-  //   },
-  //   headers: {
-  //     'x-rapidapi-key': 'c9aabb0847mshfb8c3e00e99677fp1ac30bjsn0b415fdfcc47',
-  //     'x-rapidapi-host': 'jsearch.p.rapidapi.com'
-  //   }
-  // };
   
-  // useEffect(()=>{
-  //   const getjobs= async ()=>{
-      
-      
-      
-  //     try {
-  //       const response = await axios.request(options);
-  //       console.log(response.data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
-  //   getjobs();
-  // },[])
 
 useEffect(()=>{
   const job= async()=>{
-       const jobdata = await baseapi('/search')
+    try {
+      const jobdata = await baseapi('/search')
        setjobs(jobdata.data)
-       //console.log(jobs)
+       if(jobdata.data.length>0){
+        setSelectedJob(jobdata.data[0])  //set the first job as default selected job
+        dispatch(selectedJob(jobdata.data[0].job_id))
+       }
+      
+    } catch (error) {
+      console.error('error fetching jobs',error)
+      
+    }
+       
+       console.log(jobs)
        
       
        
   }
   job();
 },[])
+const savejobid=(job)=>{
+  setSelectedJob(job)
+  
+  dispatch(selectjob(job.job_id))
+
+}
 
   return (
     <div>
         <AuthNavbar/>
-        <div className='w-[80%] m-auto'>
+        <div className='flex'>
+        <div className='w-[50%] m-auto '>
             {
                 jobs && jobs.map((job) => (
-                    <div key={job.id}>
-                        <Jobcard title={job.job_title} companyName={job.employer_name} remote={job.job_is_remote} applicationLink={job.job_apply_link} postdate={job.job_posted_at_datetime_utc} />
+                    <div key={job.job_id} onClick={()=>savejobid(job) } className="cursor-pointer">
+                        <Jobcard  title={job.job_title} companyName={job.employer_name} remote={job.job_is_remote} applicationLink={job.job_apply_link} postdate={job.job_posted_at_datetime_utc} />
+                        {/* <Jobdetails title={job.job_title} companyName={job.employer_name} remote={job.job_is_remote} applicationLink={job.job_apply_link} postdate={job.job_posted_at_datetime_utc} description={job.job_description} responsibilities={job.job_highlights.Responsibilities} requirements={job.job_highlights.Qualification} jobtype={job.job_employment_type} city={job.job_city}/> */}
                     </div>
                 ))
             }
+            
+            
+        </div>
+
+        <div className='w-1/2'>
+          {selectedJob && (
+            <Jobdetails
+              title={selectedJob.job_title}
+              companyName={selectedJob.employer_name}
+              remote={selectedJob.job_is_remote}
+              applicationLink={selectedJob.job_apply_link}
+              postdate={selectedJob.job_posted_at_datetime_utc}
+              description={selectedJob.job_description}
+              responsibilities={selectedJob.job_highlights?.Responsibilities}
+              requirements={selectedJob.job_highlights?.Qualification}
+              jobtype={selectedJob.job_employment_type}
+              city={selectedJob.job_city}
+            />
+          )}
         </div>
         
-        
+        </div> 
         
       
     </div>
