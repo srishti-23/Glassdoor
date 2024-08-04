382eead47e3e23e106a24673ede4b3322d644637
holkarajitgyandev <139675885+holkarajitgyandev@users.noreply.github.com>
2024-08-04 12:44:18 +0530

    Updated Jobdetails.jsx

--- glassdoor/src/Component/Jobdetails.jsx
+++ glassdoor/src/Component/Jobdetails.jsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import { CiBookmark } from "react-icons/ci";
-const Jobdetails = () => {
+const Jobdetails = ({title,companyName, remote, applicationLink, postdate, description, responsibilities, requirements, jobtype, city}) => {
   return (
     <div>
-        <div className='flex flex-col w-2/3  border border-black rounded'>
+        <div className='flex flex-col w-3/4  border border-black rounded bg-slate-50'>
               <div className='flex justify-between'>
                    <div className='flex flex-col gap-3 p-3 bg-slate-50'>
-                    <p>company_name</p>
-                    <p className='font-bold text-3xl'>job_title</p>
-                    <p>Remote</p>
+                    <p>{companyName}</p>
+                    <p className='font-bold text-3xl'>{title}</p>
+                    <p>Remote:{remote}</p>
                     </div>
-
+   
                     <div className='flex gap-5 p-3 '>
                         <CiBookmark/>
-                        <a  className='bg-green-400 border-black rounded text-center' href="">Easily Apply</a>
+                        <a  className='bg-green-400 border-black rounded text-center m-auto' href={applicationLink}>Easily Apply</a>
 
                     </div>
-              </div><br /><br /><br />
+              </div><br /><br />
 
 
               <div className='flex flex-col gap-2 p-3'>
-                <p>job Description:</p>
-                <p>Responsibilities:</p>
-                <p>Requirements:</p>
-                <p>Job types:</p>
-                <p>Contract length:</p>
-                <p>pay:</p>
-                <p>other:</p>
+                <p className='font-bold'>job Description:</p><p>{description}</p>
+                <p className='font-bold'>Responsibilities:</p><p>{responsibilities}</p>
+                <p>Requirements:{requirements}</p>
+                <p>Job types:{jobtype}</p>
+                
+                
+                <p>city:{city}</p>
 
               </div>
               <br /><br />
